10f9b0157440f213e22ef67b7fd822912116142b
David Porkka <dandpor@gmail.com>
2020-06-06 21:28:29 -0600

    removed duplicate 5 minute health content

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,6 @@ const Index = () => {
       <a href="https://www.nulango.com"><h2>Nulango</h2></a>
       <p>My brother and I are building this web app to help people quickly learn a new language by sharing the most common words and phrases.</p>
       <a href="https://5minutehealth.com"><h2>5 Minute Health</h2></a>
-      <a href="https://5minutehealth.com"><h2>5 Minute Health</h2></a>
       <p> After working in a grocery store and seeing the difference nutrition made in my life and other peoples’ lives, I decided to share what I’ve learned about health through this website.</p>
       <a href="https://ochemshop.com"><h2>O-Chem Shop</h2></a>
       <p>I built this site to provide organic chemistry products and resources to chemistry students and professionals.</p>
