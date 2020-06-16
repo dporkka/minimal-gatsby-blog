743a4c83aec5c57b633475dc2eba45339907d396
David Porkka <dandpor@gmail.com>
2020-06-16 17:26:58 -0500

    added analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,26 @@
 import React from "react"
 import Layout from "../components/layout"
 import Metadata from "../components/metadata"
+// import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+
+// class MyComponent extends React.Component {
+//   render() {
+//     return (
+//       <ThemeToggler>
+//         {({ theme, toggleTheme }) => (
+//           <label>
+//             <input
+//               type="checkbox"
+//               onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+//               checked={theme === 'dark'}
+//             />{' '}
+//             Dark mode
+//           </label>
+//         )}
+//       </ThemeToggler>
+//     )
+//   }
+// }
 
 const Index = () => {
   return (
