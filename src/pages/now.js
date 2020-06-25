b55640f231f8b6e2f451c78aefe1d09e762b1d1a
David Porkka <dandpor@gmail.com>
2020-06-25 15:06:41 -0500

    edited now page

--- src/pages/now.js
+++ src/pages/now.js
@@ -7,23 +7,22 @@ const Now = () => {
     <Layout>
       <Metadata title="Now" description="What I am doing now" />
       <div className="now-section">
-      <p>Here&rsquo;s what I&rsquo;m up to now.</p>
-      <p>(June 2020)</p>
+      <h2>What I&rsquo;m up to now:</h2>
 <p><strong>Working on</strong>&nbsp;</p>
-<p>Nulango, a language learning platform with my brother and this site.</p>
+<p><a href="https://www.nulango.com">Nulango</a>, a language learning web app with my brother and this site.</p>
 
 <p><strong>Recent Adventures</strong></p>
-<p>My family took a road trip out to the Badlands National Park in South Dakota and did some hiking. If you haven't seen the Badlands before you should check them out, they're magestic. After driving through hundreds of miles of flat fields and farms, all of a sudden the canyons just explode into view like a massive dried up prehistoric seabed (which they are). After a few days of hiking steep inclines on these seabeds and through rattlesnake filled trails we then got back in the car and drove to Colorado to see my brother, and do some more hiking in Estes Park. </p>
+<p>This month my family took a road trip out to the <a href="https://en.wikipedia.org/wiki/Badlands_National_Park">Badlands National Park</a> in South Dakota and did some hiking. If you haven't seen the Badlands before you should check them out. The best way that I would describe them is a magestic maze of colorful maze of dried-up, ancient seabeds that also are surrounded by grass prairies, buttes, and mountainous spires. It was an odd experience, hiking in them, becuase I felt like I was simultaneously hiking a mountain and sandy seabed. After a few days of hiking the steep inclines on these seabeds and through rattlesnake-filled trails, we then got back in the car and drove to Colorado to see my brother and do some more hiking in Estes Park.</p>
 
-<p>Estes Park is a true hikers paradise, it has all the trails you could ever hike and more. Being in the Rocky Mountain National Park, you might think they're just a bunch of rocky mountains, but you would be greatly mistaken. Estes park has a little bit of everything, from mountain lakebeds and waterfalls, to grassy slopes with marmot caves, and 14,000 foot climbs that some people where helmets, use hiking poles and wear spiked mountain boots. Unlike the trail masters, I was just wearing some running shoes, which worked out, but I definately should have been wearing some hiking shoes on some parts of the trail. The only thing that I didn&rsquo;t like about that trip was the 14-hour ride on the way back to Wisconsin that started in the afternoon and went through the night. That was a bit too many sleepless hours in the car.&nbsp;</p>
+<p>Estes Park is a true hikers paradise—it has all the trails you could ever hike and more. Being in the <a href="https://en.wikipedia.org/wiki/Rocky_Mountain_National_Park">Rocky Mountain National Park</a>, you might think they're just a bunch of rocky, mountain trails, but you would be greatly mistaken. Estes park is very diverse and has everything from mountain lakebeds and waterfalls, to grassy slopes with marmot caves, and 14,000 foot climbs that some people use hiking poles, wear helmets, and spiked mountain boots for. Unlike the pro mountaineers, I was just wearing some running shoes, but I definately should have been wearing some hiking shoes on some parts of the trail. The only thing that I didn&rsquo;t like about that trip was the 14-hour ride on the way back to Wisconsin that started in the afternoon and went through the night. That was a bit too many sleepless hours in the car.&nbsp;</p>
 <p><strong>New Discoveries</strong></p>
-<p>During the road trip, we happened to stop by some gas stations along the way and I rediscovered the flavor-packed, crunchy, salty, well-seasoned goodness that exists in a bag of dry-roasted, Planters peanuts. I&rsquo;ve since been eating these consistently, and probably excessively, as a mid-day, mid-night, and mid-morning snack.&nbsp;</p>
+<p>Dry-roasted, Planters peanuts. Since picking up a bag at some gas station in Wyoming, I&rsquo;ve  been eating these consistently, and probably excessively, as a mid-day, mid-night, and mid-morning snack.&nbsp;</p>
 <p><strong>Exercise&nbsp;</strong></p>
-<p>The scrawny to brawny transformation is underway. This month, I finally picked up a jump rope again, after about 7-10 years of not jump roping. I was really surprised by the workout I had and my feet were still sore the next few days! I also started to run again after not running for a few months. My current running strategy is to run at like 80% effort, then walk for a minute or two, and repeat, usually in quarter-mile increments, but I&rsquo;m slowly starting to build my stamina again to run more than a quarter-mile at a time.&nbsp;</p>
+<p>The scrawny to brawny transformation is underway. This month, I finally picked up a jump rope again, after about 7-10 years of not jump roping. I was really surprised by the workout I had and my feet were still sore the next few days! I also started to run again after not running for a few months. My current running strategy is to run at like 80% effort until I'm tired, then walk for a minute or two, and repeat, usually in quarter-mile increments. I&rsquo;m slowly starting to build my stamina again to run more than a quarter-mile at a time.&nbsp;</p>
 <p> I&rsquo;ve also been doing a lot of pull-ups, using TRX straps to do ab work and cardio, and lifting weights to build some muscle. </p>
 <p><strong>TV Shows</strong></p>
 
-<p>I haven&rsquo;t been watching much TV lately, but I finished season 4 of <em>Money Heist</em>, which is a show about two elaboratively planned, action-packed, nerve-racking bank heists in Spain. I started watching this show mostly to improve my Spanish, but continued to watch because it was sooo good. Every scene in this show is masterfully crafted and the acting is the best I&rsquo;ve seen. I can&rsquo;t wait for the final season to be released.</p>
+<p>I haven&rsquo;t been watching much TV lately, but I finished season 4 of <em>Money Heist</em>, which is a show about two elaboratively planned, action-packed, nerve-racking bank heists in Spain. I started watching this show mostly to improve my Spanish, but continued to watch because it was sooo good. Every scene in this show is masterfully crafted and the acting is some of the best I&rsquo;ve seen. I can&rsquo;t wait for the final season to be released.</p>
 <p>Another addicting show that I&rsquo;ve been catching up on is the futuristic, sci-fi show, <em>Altered Carbon</em>, where people can transfer their consciousness into another body via &ldquo;stacks&rdquo; in their neck and live forever, or at least until someone destroys their stack. I&rsquo;ve watched a lot of sci-fi over the years and I would rate this show 10/10.&nbsp;</p>
 
 <p><strong>Movies </strong>(spoiler alert orange)</p>
@@ -37,20 +36,27 @@ const Now = () => {
 
 <p><strong>Music</strong></p>
 <p><em>TiedUpRightNow</em> &amp; IknowhowIfeel &mdash; Parcels&nbsp;</p>
+<iframe width="560" height="50" src="https://www.youtube.com/embed/dhAR_VnEAZ8" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="50" src="https://www.youtube.com/embed/c6vwL7XgVz4" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <p>Love the funky upbeat groove of these songs and the drawn-out vocals that match the vibe perfectly. If you like Jefferson Airplane or The Alan Parsons Project, you&rsquo;ll probably like Parcels.</p>
 
+
 <p><em>Technicolor Beat &mdash; Oh Wonder</em></p>
-<p>Big fan of the overlapping male and female vocals and the complex choreography of various background sounds that blend in an ear-soothing, technicolor beat.</p>
+<iframe width="560" height="50" src="https://www.youtube.com/embed/W8fmOciEY8U" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p>Overlapping male and female vocals, complex choreography of various background sounds that blend in an ear-soothing, technicolor beat.</p>
 
 <p><em>The Journey&nbsp; &mdash; Tom Misch</em></p>
-<p>My brother introduced me to Tom Misch and the unique, usually down-tempo, highly collaborative, English Jazz scene.&nbsp;</p>
+<iframe width="560" height="50" src="https://www.youtube.com/embed/LAxcxUOZcDU" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p>Tom Misch's songs are very diverse, but this one is down-tempo, instrumental, contemplative, and representative of the south London jazz scene, which I just starting to appreciate after my brother introduced me to it.</p>
 
 <p><em>A Scent of Lily &ndash; Augustine&nbsp;</em></p>
-<p>I&rsquo;ve listened to this song a lot this month and still can&rsquo;t relate to the lyrics, but love the vocal range of the singer and the constantly changing pitch which builds nicely with melody. Listen to the first minute of it before you decide it&rsquo;s not your thang.&nbsp;</p>
+<iframe width="560" height="50" src="https://www.youtube.com/embed/HaBYoEjEo2M" frameborder="0" allow="accelerometer; encrypted-media; gyroscope;" allowfullscreen="false"></iframe>
+<p>I&rsquo;ve listened to this song a lot this month and still can&rsquo;t relate to the lyrics, but love the vocal range of the singer and the constantly changing pitch which builds nicely with melody. Listen to the first minute of it before you decide it&rsquo;s not for you.&nbsp;</p>
 
 <p><strong>Current Beverages</strong></p>
 <p>Dark roast coffee by kicking horse. Despite my primitive way of preparing it, by tossing the grounds in a tea filter and letting it steep, this coffee is some of the best I&rsquo;ve had. Will repeat bi-daily.</p>
 
+<p> This page was inspired by <a href="https://sivers.org/">Derek Sivers</a> and the <a href="https://nownownow.com/">Now Project</a>.</p>
     </div>
     </Layout>
   )
