ab5a05dbeb99e98207465302a2f854c1a3c59973
David Porkka <dandpor@gmail.com>
2020-08-16 14:01:26 -0500

    updated Now and Project styles and content

--- src/pages/now.js
+++ src/pages/now.js
@@ -7,35 +7,36 @@ const Now = () => {
     <Layout>
       <Metadata title="Now" description="What I am doing now" />
       <div className="now-section">
-      <h2>What I&rsquo;m up to now:</h2>
-<p><strong>Working on</strong>&nbsp;</p>
+      <h2><strong>What I&rsquo;m up to now:</strong></h2>
+<p className="p-section"><strong>Working on</strong>&nbsp;</p>
 <p><a 
 href="https://www.nulango.com">Nulango</a>, a language learning web app with my brother, and adding some new content and features to this site.</p>
 
-<p><strong>Recent Adventures</strong></p>
+<p className="p-section"><strong>Recent Adventures</strong></p>
 
 <p>Took a roadtrip to Pictured Rock National Park in Wisconsin and did some hiking, then continued the road trip up to the Upper Peninsula in Michigan and snorkeled for agates in the clear, cold waters of Lake Superior. Highly recommend both activities.
 </p>
 
-<p><strong>New Routines</strong></p>
+<p className="p-section"><strong>New Routines</strong></p>
 
 <p>Trying to take more deep belly breaths to better oxygenate and improve lung elasticity. I'll report later on the effectiveness on this new breathing routine.</p>
 
 <p>Last month I started experimenting with short duration high-intensity running, and have contnued to do that for most of the month. Overall, I didn't notice a big difference in endurance from these shorter runs, but maybe I'm running faster, probably should be measuring my speed to be more objective.</p>
 
-<p><strong>Current Music</strong></p>
+<p className="p-section"><strong>Current Music</strong></p>
 
 <p>Vision by Atmosphere</p>
 <p>This instrumental song starts slowly with a mysterious synth sound and gradually builds into a motivating and immersive war drum with a stimulating electronic beat that makes you want to explore. I recommend playing this song before you step into a forest or try listening to it to make boring things seem more interesting.</p>
 
-<p><strong>Learning</strong></p>
+<p className="p-section"><strong>Learning</strong></p>
 
-<p>React JS (Javascript library),&nbsp; Amazon Web Services, and Mandarin.</p>
+<p>React JS (Javascript library),&nbsp; Amazon Web Services, 11ty static site generator, and Mandarin.</p>
 
-<p><strong>Latest Good Reads</strong></p>
+<p className="p-section"><strong>Latest Good Reads</strong></p>
 
 <p><a href="https://waitbutwhy.com/">Wait But Why Articles</a></p>
-<p>Been binge reading articles on this wide-ranging, sometimes informative, often profound, yet light-hearted site. </p>
+<p>Been binge reading articles on this wide-ranging, sometimes informative, often profound, yet usually light-hearted site. </p>
+<p className="updated"><i>Updated August 2020</i></p>
 <hr className="now-line"></hr>
 
 <p><strong>The Now Archive:</strong></p>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,19 +5,19 @@ import Metadata from "../components/metadata"
 const Projects = () => {
   return (
     <Layout>
-      <Metadata title="About" description="Get to know me" />
+      <Metadata title="Projects" description="What I'm working on" />
       <div className="projects-section">
-      <h2 className="projects-h2">Projects</h2>
+      <h2 className="current-projects"><strong>Current Projects</strong></h2>
       <a className="nulango-projects" href="https://www.nulango.com"><h2>Nulango</h2></a>
       <p>Web app to help people quickly learn a language</p>
-
-      <a className="5mh-projects"href="https://5minutehealth.com"><h2>5 Minute Health</h2></a>
-      <p>Health and nutrition website with a focus on brain health and lengthening healthspan</p>
       <a className="ochemshop-projects" href="https://ochemshop.com"><h2>O-Chem Shop</h2></a>
       <p>Organic chemistry products and resources for chemistry students and professionals
       </p>
       <a href="/" className="project2"><h2>This Site</h2></a>
       <p>Adding new features and content to this Gatsby static site. The code is available on <a href="https://github.com/dporkka/minimal-gatsby-blog" className="link1">GitHub</a>.</p>
+      <h2 className="on-hold-projects"><strong>On Hold</strong></h2>
+      <a className="5mh-projects"href="https://5minutehealth.com"><h2>5 Minute Health</h2></a>
+      <p>Health and nutrition website with a focus on brain health and lengthening healthspan</p>
       <a href="https://docs.google.com/spreadsheets/d/1ZULEa5NRBXssyZtWWEkaEXnnUQZogtAV2ZX4j75tW5o/edit?usp=sharing" className="project2"><h2>Income and Expense Tracker (Google Sheet)</h2></a>
       <p>Built this to keep track of recurring income and expenses. A free, editable version of this program is available <a href="https://docs.google.com/spreadsheets/d/1ZULEa5NRBXssyZtWWEkaEXnnUQZogtAV2ZX4j75tW5o/copy" className="link1">here</a>.</p>
     </div>
